89579bf2e3c18347461cc05862cb5b33707354cc
capcesar <cesar_a_pereira@hotmail.com>
2023-07-10 20:31:19 -0300

    Estudo spring e java 8

---
+++ src/test/java/br/com/marmitas/controle/ControleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marmitas.controle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
